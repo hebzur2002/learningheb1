545b46052a1c3fefaa66fc91c4dfbd78092a4195
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 02:31:19 +0530

    Add About component to display personal information

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+// Ye component "About Me" section dikhata hai
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>Ye mera pehla React website hai jo maine khud banaya hai.</p>
+    </section>
+  )
+}
+
+export default About
