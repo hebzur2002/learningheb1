70bc8ab5462bb2da027c71cab4688eb4bb1910b9
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 02:40:57 +0530

    Add a new Contact page component

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,11 @@
+// Ek naya Contact page - simple example
+function Contact() {
+  return (
+    <section className="about">
+      <h2>Contact Me</h2>
+      <p>Email: yourname@example.com</p>
+    </section>
+  )
+}
+
+export default Contact
