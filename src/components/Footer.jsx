1b0323b92762003a8c6ae6123329f455a06ff240
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 02:32:08 +0530

    Add Footer component with copyright information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// Ye component copyright wali footer line dikhata hai
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 My Website. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
