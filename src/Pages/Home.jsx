68a83ec763d4bc93f62728dd8e5a08b2e42304cf
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 02:40:17 +0530

    Add Home component with Header and About

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header.jsx'
+import About from '../components/About.jsx'
+
+// Home page pe Header + About dikhega
+function Home() {
+  return (
+    <>
+      <Header />
+      <About />
+    </>
+  )
+}
+
+export default Home
