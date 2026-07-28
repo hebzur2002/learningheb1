4027aecf1d30fb3d8658aa623b070f5b441619af
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 16:56:20 +0530

    Implement NotFound route for unmatched URLs
    Added a NotFound route to handle unmatched URLs.

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Contact from './pages/Contact.jsx'
+import NotFound from './pages/NotFound.jsx'   // naya import
 import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    // BrowserRouter pura app ko "routing" ki power deta hai
     <BrowserRouter>
       <div className="container">
-        {/* Navigation menu - Link se page change hota hai bina reload ke */}
         <nav>
           <Link to="/">Home</Link> | <Link to="/contact">Contact</Link>
         </nav>
 
-        {/* Routes batata hai kaunse URL pe kaunsa page dikhana hai */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />   {/* naya add kiya */}
           <Route path="/contact" element={<Contact />} />
+          {/* Ye "catch-all" route hai - jo bhi URL match na ho, ye dikhega */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
         <Footer />
