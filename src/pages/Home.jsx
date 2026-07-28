9f34f7b8a93994e20b4ce7d2000e9402e050a40a
hebzur2002 <hebzurtech143@gmail.com>
2026-07-28 16:23:03 +0530

    Add Counter component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Header from '../components/Header.jsx'
 import About from '../components/About.jsx'
-
+import Counter from '../components/Counter.jsx'
 // Home page pe Header + About dikhega
 function Home() {
   return (
     <>
       <Header />
       <About />
+      <Counter />
     </>
   )
 }
